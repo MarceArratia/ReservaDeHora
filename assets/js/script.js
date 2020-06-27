7b7cbe9e2ddd68a0c978904094e60c8151e78d71
MarceArratia <marratiaf@hotmail.com>
2020-06-27 12:45:48 -0400

    validaciones terminadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,44 @@
 function validar(){
     //validar nombre
-    var rut, nombre, apellidos,edad, correo, esp, especialidad,fecha, hora, ho,expresion;
+    var rut,expresionRut, nombres,expresionNombre, apellidos,expresionApellido,edad, correo, esp, especialidad,fecha, hora, ho, expresionCorreo;
     rut = document.getElementById("rut").value;
-    nombre = document.getElementById("nombre").value;
+    expresionRut = /^(\d{2}\.\d{3}\.\d{3}-)([a-zA-Z]{1}$|\d{1}$)/;
+    nombres = document.getElementById("nombres").value;
+    expresionNombre= /^[A-Za-z]+$/;
     apellidos = document.getElementById("apellidos").value;
+    expresionApellido= /^[A-Za-z]+$/;
     edad = document.getElementById("edad").value;
     correo = document.getElementById("correo").value;
+    expresionCorreo = /\w+@\w+\.+[a-z]/;
     esp = document.getElementById("especialidad");
     especialidad = esp.options[esp.selectedIndex].text;
     fecha = document.getElementById("fecha").value;
     ho = document.getElementById("hora");
     hora = ho.options[ho.selectedIndex].text;
     
-    expresion = /\w+@\w+\.+[a-z]/;
-
-    if (rut === "" || nombre === "" || apellidos === "" || correo === "" || edad === ""){
+   //Validaciones
+    if (rut === "" || nombres === "" || apellidos === "" || correo === "" || edad === "" || especialidad === "" || fecha === ""|| hora === ""){
         alert("Todos los campos son obligatorios");
         return false;
     }
-    else if (rut.length<9){
-        alert("El rut debe tener mínimo 9 digitos");
+    else if (!expresionRut.test(rut)){
+        alert("Ingrese un número de Rut válido");
         return false;
     }
-    else if (nombre.length>30){
-        alert("El nombre es muy largo");
+    else if (!expresionNombre.test(nombres)){
+        alert("Nombre inválido");
         return false;
     }
-    else if (apellidos.length>80){
-        alert("Los apellidos son muy largos");
+    else if (!expresionApellido.test(apellidos)){
+        alert("Apellido inválido");
         return false;
     }
-    else if (correo.length>100){
-        alert("El correo es muy largo");
-        return false;
-    }
-    else if (!expresion.test(correo)){
+    else if (!expresionCorreo.test(correo)){
         alert("El correo no es válido");
         return false;
     }
    
-   alert("Estimado(a) " + nombre+" "+apellidos+" su hora para "+especialidad+ " ha sido reservada para el día "+fecha+" a las " +hora+" Además, se le envió un mensaje a su correo: "+correo+" con el detalle de su cita"+`<p>${"Gracias por preferirnos"}</p>`);
+   document.write("Estimado(a) " + nombres+" "+apellidos+" su hora para "+especialidad+ " ha sido reservada para el día "+fecha+" a las " +hora+" Además, se le envió un mensaje a su correo: "+correo+" con el detalle de su cita"+`<p><center>${"¡¡¡Gracias por preferirnos!!!"}</center></p>`);
     
     
 }
