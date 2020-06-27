c86966fc4086e171c104026c08f394d4d44498ee
MarceArratia <marratiaf@hotmail.com>
2020-06-26 23:50:39 -0400

    validando datos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,25 @@
 function validar(){
     //validar nombre
-    var nombre, apellidos, correo, usuario,pass, telefono, expresion;
+    var rut, nombre, apellidos,edad, correo, esp, especialidad,fecha, hora, ho,expresion;
+    rut = document.getElementById("rut").value;
     nombre = document.getElementById("nombre").value;
     apellidos = document.getElementById("apellidos").value;
+    edad = document.getElementById("edad").value;
     correo = document.getElementById("correo").value;
-    usuario = document.getElementById("usuario").value;
-    pass = document.getElementById("pass").value;
-    telefono = document.getElementById("telefono").value;
+    esp = document.getElementById("especialidad");
+    especialidad = esp.options[esp.selectedIndex].text;
+    fecha = document.getElementById("fecha").value;
+    ho = document.getElementById("hora");
+    hora = ho.options[ho.selectedIndex].text;
+    
     expresion = /\w+@\w+\.+[a-z]/;
 
-    if (nombre === "" || apellidos === "" || correo === "" || usuario === ""|| pass === "" || telefono === ""){
+    if (rut === "" || nombre === "" || apellidos === "" || correo === "" || edad === ""){
         alert("Todos los campos son obligatorios");
+        return false;
+    }
+    else if (rut.length<9){
+        alert("El rut debe tener mínimo 9 digitos");
         return false;
     }
     else if (nombre.length>30){
@@ -29,19 +38,9 @@ function validar(){
         alert("El correo no es válido");
         return false;
     }
-    else if (usuario.length>20){
-        alert("El usuario debe tener 20 carácteres como máximo");
-        return false;
-    }
-    else if (telefono.length>15){
-        alert("El telefono es muy largo");
-        return false;
-    }
-    else if (isNaN(telefono)){
-        alert("Ha ingresado un  número de teléfono no válido");
-        return false;
-    }
    
-    alert("Hola " + nombre+" "+apellidos+" tu nombre de usuario es "+usuario+ " y tu contraseña es "+pass);
-
+   alert("Estimado(a) " + nombre+" "+apellidos+" su hora para "+especialidad+ " ha sido reservada para el día "+fecha+" a las " +hora+" Además, se le envió un mensaje a su correo: "+correo+" con el detalle de su cita"+`<p>${"Gracias por preferirnos"}</p>`);
+    
+    
 }
+document.getElementById('sendReserv').addEventListener('click', validar);
